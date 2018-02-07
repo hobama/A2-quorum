aca502952f9ef69746dbb5ed8bfd2b79167353ea
Konstantinos Peratinos <kotsosgr@gmail.com>
2018-02-07 04:24:03 +0100

    modified sim script

--- a2-synchrotron-contiki/tools/cooja/runsim.sh
+++ a2-synchrotron-contiki/tools/cooja/runsim.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo ant run_nogui -Dargs='../../../../sim20.1.csc -contiki=../../../'
-sudo ant run_nogui -Dargs='../../../../sim20.3.csc -contiki=../../../'
-sudo ant run_nogui -Dargs='../../../../sim6.2.csc -contiki=../../../'
+sudo ant run_nogui -Dargs='../../../../Simulation-files/sim24.1.csc -contiki=../../../'
+sudo ant run_nogui -Dargs='../../../../Simulation-files/sim24.2.csc -contiki=../../../'
+sudo ant run_nogui -Dargs='../../../../Simulation-files/sim12.1.csc -contiki=../../../'
+sudo ant run_nogui -Dargs='../../../../Simulation-files/sim12.2.csc -contiki=../../../'
 
 
 
